b40cb898454437966c5c32c89416b824fb0173ac
Nick Fagerlund <nick.fagerlund@gmail.com>
2024-02-03 14:12:17 -0800

    Initial axum version of ghastbook, browse to /:visitor for fun
    Plain / doesn't work yet bc that's considered a different route by the router, turns out.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    routing::get,
+    Router,
+};
 use clap::Parser;
 use sqlx::{query, query_as, sqlite::SqlitePool, FromRow};
 
@@ -31,22 +37,49 @@ async fn fetch(pool: &SqlitePool) -> Result<Vec<Visitation>, sqlx::Error> {
     query_as::<_, Visitation>(FETCH).fetch_all(pool).await
 }
 
+async fn web_visit(
+    Path(visitor): Path<String>,
+    State(pool): State<SqlitePool>,
+) -> Result<String, StatusCode> {
+    // ok so this is basically our "main" now, logic wise
+
+    let current = if visitor.is_empty() {
+        "default buttmunch"
+    } else {
+        &visitor
+    };
+    // Do the visit, maybe barf.
+    if (visit(&pool, current, 1).await).is_err() {
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    }
+
+    // Get the leaderboard and return it
+    match fetch(&pool).await {
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+        Ok(visits) => {
+            let resp = visits
+                .iter()
+                .map(|v| format!("{}: {}\n", v.visitor, v.count))
+                .fold(String::new(), |mut acc, line| {
+                    acc.push_str(&line);
+                    acc
+                });
+            Ok(resp)
+        }
+    }
+}
+
 // OK, baby's first tokio app...
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let args = Cli::parse();
-
     let pool = SqlitePool::connect("sqlite:./ghastbook.db").await?;
-    let current = &args.visitor;
 
-    // ok setup time's over baby
-    // visit:
-    visit(&pool, current, 1).await?;
-    // dump:
-    let results = fetch(&pool).await?;
-    for Visitation { visitor, count } in results.iter() {
-        println!("{}: {}", visitor, count);
-    }
+    let app = Router::new()
+        .route("/:visitor", get(web_visit))
+        .with_state(pool.clone());
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 
     Ok(())
 }
