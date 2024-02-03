206a3fe1ba3ceca7df894b3e41e4f77528423ae3
Nick Fagerlund <nick.fagerlund@gmail.com>
2024-02-03 14:39:39 -0800

    Eh just consume the pool

--- src/main.rs
+++ src/main.rs
@@ -82,7 +82,7 @@ async fn main() -> anyhow::Result<()> {
     let app = Router::new()
         .route("/:visitor", get(web_visit))
         .route("/", get(empty_web_visit))
-        .with_state(pool.clone());
+        .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
