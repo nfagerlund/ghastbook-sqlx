930ed04a504169445fc671e7b20525dce5268e89
Nick Fagerlund <nick.fagerlund@gmail.com>
2024-02-03 14:40:13 -0800

    Add some tracing events and logging... IRL, I'd want to log to a logrotated file prolly

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use axum::{
 };
 use clap::Parser;
 use sqlx::{query, query_as, sqlite::SqlitePool, FromRow};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -77,6 +78,21 @@ async fn empty_web_visit(state: State<SqlitePool>) -> Result<String, StatusCode>
 // OK, baby's first tokio app...
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // this tracing stuff... woof... anyway, it looks like registry() makes a
+    // blank-ish subscriber that you can insert a bunch of layers into, the .with()
+    // calls insert layers that can filter events or choose to emit them to some
+    // form of output, and the .init() makes the finished subscriber the default
+    // subscriber. And, for some reason, my crate name gets transformed to have an
+    // underscore instead of a hyphen, not sure what the rules for that are,
+    // maybe it's in the info!() macro somewhere? Anyway, the EnvFilter thing lets you
+    // set a string like the one you see inline via the $RUST_LOG env var.
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "ghastbook_sqlx=debug,sqlx=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
     let pool = SqlitePool::connect("sqlite:./ghastbook.db").await?;
 
     let app = Router::new()
@@ -85,6 +101,7 @@ async fn main() -> anyhow::Result<()> {
         .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::info!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 
     Ok(())
