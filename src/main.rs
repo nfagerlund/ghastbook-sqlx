5af89b00b7791ef1d88b400e0688697511bc5a57
Nick Fagerlund <nick.fagerlund@gmail.com>
2024-02-14 22:43:54 -0800

    Switch to using compile-time checked query macros
    OK yeah I was initially dubious but that's actually fucking sick.
    
    The only main downside is that they NEED string literals, they can't refer
    to strings in a constant. That's all right tho, just de-duplicate at the macro
    level by wrapping queries in functions.

--- src/main.rs
+++ src/main.rs
@@ -31,23 +31,28 @@ struct Visitation {
     count: i64,
 }
 
-// Reversing the params order to test explicit positional. fix later maybe.
-const VISIT: &str = r#"
-    INSERT INTO visits (visitor, count) VALUES (?2, ?1)
-    ON CONFLICT(visitor) DO UPDATE
-    SET count = count + ?1;
-    "#;
-const FETCH: &str = r#"
-    SELECT visitor, count FROM visits ORDER BY count DESC;
-    "#;
-
 async fn visit(pool: &SqlitePool, visitor: &str, times: i64) -> anyhow::Result<()> {
-    query(VISIT).bind(times).bind(visitor).execute(pool).await?;
+    query!(
+        r#"
+        INSERT INTO visits (visitor, count) VALUES (?1, ?2)
+        ON CONFLICT(visitor) DO UPDATE
+        SET count = count + ?1;
+        "#,
+        visitor,
+        times
+    )
+    .execute(pool)
+    .await?;
     Ok(())
 }
 
 async fn fetch(pool: &SqlitePool) -> Result<Vec<Visitation>, sqlx::Error> {
-    query_as::<_, Visitation>(FETCH).fetch_all(pool).await
+    query_as!(
+        Visitation,
+        "SELECT visitor, count FROM visits ORDER BY count DESC;"
+    )
+    .fetch_all(pool)
+    .await
 }
 
 async fn web_visit(
